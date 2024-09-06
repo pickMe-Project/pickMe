6528fc56ab3df84dbc02737fef8ca1b741ebc6e8
ChristoNP <chris.pontoh@gmail.com>
2024-09-06 07:48:12 +0700

    collapsible chatbot button

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -90,17 +90,40 @@ const Chat = () => {
       </AnimatePresence>
       <motion.button
         onClick={() => setIsOpen(!isOpen)}
-        className="bg-yellow-400 text-black w-16 h-16 rounded-full shadow-lg flex items-center justify-center hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-yellow-400 transition-all duration-300"
-        whileHover={{ scale: 1.1 }}
+        className="bg-yellow-400 text-black w-16 h-16 rounded-full shadow-lg flex items-center justify-center hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-yellow-400 transition-all duration-300 overflow-hidden"
+        whileHover={{ scale: 1.1, rotate: 360, transition: { duration: 0.5 } }}
         whileTap={{ scale: 0.9 }}
+        animate={{
+          backgroundColor: isOpen ? ['#FBBF24', '#FCD34D', '#FBBF24'] : '#FBBF24',
+          transition: { duration: 2, repeat: Infinity }
+        }}
       >
-        {isOpen ? (
-          <FaChevronDown className="text-2xl" />
-        ) : (
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
-            <path fillRule="evenodd" d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" clipRule="evenodd" />
-          </svg>
-        )}
+        <motion.div
+          initial={false}
+          animate={{
+            rotate: isOpen ? 180 : 0,
+            scale: isOpen ? [1, 1.2, 1] : 1,
+            transition: { duration: 0.5 }
+          }}
+        >
+          {isOpen ? (
+            <FaChevronDown className="text-2xl" />
+          ) : (
+            <motion.svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-8 h-8"
+              animate={{
+                scale: [1, 1.1, 1],
+                rotate: [0, 10, -10, 0],
+                transition: { duration: 2, repeat: Infinity }
+              }}
+            >
+              <path fillRule="evenodd" d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" clipRule="evenodd" />
+            </motion.svg>
+          )}
+        </motion.div>
       </motion.button>
     </div>
   );
