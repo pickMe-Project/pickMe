d9720fadd38c89446acb0558d277c2798d325a47
ChristoNP <chris.pontoh@gmail.com>
2024-09-09 13:37:35 +0700

    fixing env config

--- client/env.d.ts
+++ client/env.d.ts
@@ -2,6 +2,7 @@ namespace NodeJS {
   interface ProcessEnv {
     DATA_BASE_URL: string;
     JWT_SECRET: string;
+    DB_NAME: string;
     NEXT_PUBLIC_BASE_URL: string;
     OPEN_AI_API_KEY: string;
   }
