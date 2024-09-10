a7a642a01afad05d02ee9765286cc74b710440e1
Ghifary Adinegoro <ghifaryadinegoro555@gmail.com>
2024-09-10 14:31:41 +0700

    fix protected route add song

--- client/src/components/AddSongToCourse.tsx
+++ client/src/components/AddSongToCourse.tsx
@@ -33,11 +33,17 @@ export default function AddSongToCourse({ song }: Props) {
             "Content-Type": "application/json",
           },
         });
-        //   console.log(response, "<<<<<<<<< responseComponentAddToSongCourse");
+          console.log(response, "<<<<<<<<< responseComponentAddToSongCourse");
 
         if (!response.ok) {
           const errorBody = await response.json();
 
+          if (
+            errorBody.error === "Invalid Token" ||
+            errorBody.error === "Please subscribe to PickMe!"
+          ) {
+            router.push(`/songs/${song.slug}/course`);
+          }
           if(errorBody.error === "Invalid Token") {
             router.push(`/songs/${song.slug}/course`);
           }
